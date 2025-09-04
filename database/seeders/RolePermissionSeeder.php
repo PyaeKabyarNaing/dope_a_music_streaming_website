ea25068615625ae32ad037b25ad820cc3f596a34
PyaeKabyarNaing <pyaekabyarwork@gmail.com>
2025-09-04 14:37:21 +0630

    logo added

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -20,12 +20,20 @@ class RolePermissionSeeder extends Seeder
         Permission::create(['name' => 'delete songs']);
         Permission::create(['name' => 'edit songs']);
         Permission::create(['name' => 'view songs']);
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'ban users']);
+        Permission::create(['name' => 'delete users']);
+        Permission::create(['name' => 'view users']);
+        Permission::create(['name' => 'update users']);
 
         // Create roles and assign existing permissions
         $role = Role::create(['name' => 'listener']);
         $role->givePermissionTo('view songs');
 
         $role = Role::create(['name' => 'artist']);
+        $role->givePermissionTo('view songs', 'create songs', 'edit songs', 'delete songs');
+
+        $role = Role::create(['name' => 'admin']);
         $role->givePermissionTo(Permission::all());
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,17 +3,11 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -26,6 +20,8 @@
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
+
+                                {{-- dropdown arrow icon --}}
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
