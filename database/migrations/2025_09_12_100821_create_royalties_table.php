42a79a5e20b51ed4169986a09e46c5eff20998b8
PyaeKabyarNaing <pyaekabyarwork@gmail.com>
2025-09-12 16:44:05 +0630

    database update royalties

---
+++ database/migrations/2025_09_12_100821_create_royalties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('royalties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('artist_id')
+            ->constrained('users')
+            ->onDelete('cascade');
+            $table->integer('total_streams');
+            $table->decimal('amount');
+            $table->date('period_start');
+            $table->date('period_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('royalties');
+    }
+};
